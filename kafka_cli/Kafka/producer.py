65f81aacbfb03211e84a7bc55f96e058dcf182c6
Abhishek Bose <abose550@gmail.com>
2021-01-12 01:21:23 +0530

    producer message passing tested

---
+++ kafka_cli/Kafka/producer.py
@@ -0,0 +1,32 @@
+from confluent_kafka import Producer
+import json
+
+class producer:
+    def __init__(self,topic,broker="localhost:9092",partition=None):
+        self.broker = broker
+        self.partition = partition
+        self.prod = Producer(
+            {
+                'bootstrap.servers': self.broker,
+            }
+        )
+        self.topic = topic
+    
+    def __delivery_report(self,err, msg):
+        """ Called once for each message produced to indicate delivery result.
+            Triggered by poll() or flush(). 
+        """
+        if err is not None:
+            print('Message delivery failed: {}'.format(err))
+        else:
+            print('Message delivered to {} [{}]'.format(msg.topic(), msg.partition()))
+
+
+    def produce(self,msg,json_needed=True):
+        # self.prod.poll(0)
+        if json_needed:
+            x = json.dumps(msg)
+        else:
+            x =msg.encode('utf-8')
+        self.prod.produce(self.topic,x,callback=self.__delivery_report)
+        self.prod.poll(0)
