877b9c99ab945ed0aeb1303e56d43bba1aa8758d
Abhishek Bose <abose550@gmail.com>
2021-01-12 00:21:45 +0530

    Passed messages through consumer. Smoke test done

--- kafka_cli/Kafka/consumer.py
+++ kafka_cli/Kafka/consumer.py
@@ -1,7 +1,7 @@
 from confluent_kafka import Consumer
 
 class consumer:
-    def __init__(self,broker="localhost:2181",group,topic):
+    def __init__(self,topic,broker="localhost:9092",group="group1"):
         self.broker = broker
         self.group = group
         self.con = Consumer(
@@ -13,20 +13,29 @@ class consumer:
         )
         self.topic = topic
 
-        self.con.subscribe([topic])
-
+        
     def start_reading(self):
+        
+        self.con.subscribe([self.topic])
 
+        print("Going to read messages from kafka topic {}. Press CTRL + C to stop".format(self.topic))
         while True:
-            msg = c.poll(1.0)
+            try:
+                msg = self.con.poll(1.0)
 
-            if msg is None:
-                continue
-            if msg.error():
-                print("Consumer error: {}".format(msg.error()))
-                continue
+                if msg is None:
+                    continue
+                if msg.error():
+                    print("Consumer error: {}".format(msg.error()))
+                    continue
 
-            print('Received message: {}'.format(msg.value().decode('utf-8')))
+                print('Received message: {}'.format(msg.value().decode('utf-8')))
+            except KeyboardInterrupt:
+                print("Shutting consumer...")
+                # self.stop()
+                break
+            except RuntimeError:
+                break
 
     def stop(self):
         self.con.close()
