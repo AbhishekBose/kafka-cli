38a6ba5ccf5eb13967354048df7f9e4590176824
Abhishek Bose <abose550@gmail.com>
2021-01-13 01:59:58 +0530

    Added options.py page for user input for kafka config :hammer:

--- kafka_cli/cli/options.py
+++ kafka_cli/cli/options.py
@@ -1,13 +1,34 @@
 from __future__ import print_function, unicode_literals
 from PyInquirer import prompt, print_json
+from pprint import pprint
 
-questions = [
-    {
-        'type': 'input',
-        'name': 'first_name',
-        'message': 'What\'s your first name',
-    }
-]
+class options:    
+    @staticmethod
+    def option_page():
+        questions = [
+            {
+                'type': 'list',
+                'name': 'Kafka operation',
+                'message': 'What operation do you want to perform?',
+                'choices': ['Produce', 'Consume'],
+                'filter': lambda val: val.lower()
+            },
+            {
+                'type': 'input',
+                'name': 'topic',
+                'message': 'What is the name of your topic?',
+                'filter': lambda val: val.lower()
+            },
+            {
+                'type': 'input',
+                'name': 'broker',
+                'message': 'What is your broker address?',
+                'filter': lambda val: val.lower()
+            }
+        ]
 
-answers = prompt(questions)
-print_json(answers) 
+        answers = prompt(questions)
+        print(answers)
+
+if __name__ == "__main__":
+    options.option_page()
